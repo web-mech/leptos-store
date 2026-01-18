21939072185a3e2547acb91cd83c13e1702cbee2
web-mech <webmech@gmail.com>
2026-01-18 01:38:06 -0500

    docs(components): add license and copyright header to components.rs

--- examples/auth-store-example/src/components.rs
+++ examples/auth-store-example/src/components.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2026 Mike Price
+
 //! UI Components for the Auth Store Example
 //!
 //! This module provides Leptos components that demonstrate
